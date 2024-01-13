b2028ed3555dc3f6070dd992ecc31722a3980183
Top C <136592513+Abdullahciti@users.noreply.github.com>
2024-01-13 12:21:23 +0100

    Add files via upload

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,29 @@
+<template>
+  <div
+    class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+  >
+    <div class="p-4 bg-white self-start mt-32 max-w-screen-md">
+      <slot />
+      <button
+        @click="toggleModal"
+        class="close text-white mt-8 bg-weather-primary py-2 px-6"
+      >
+        Close
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["toggleModal"],
+  // data() {
+  //   return {
+  //     modalActive: false,
+  //   };
+  // },
+};
+</script>
+
+<style scoped>
+</style>
